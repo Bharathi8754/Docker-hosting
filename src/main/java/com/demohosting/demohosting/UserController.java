84679afe14e76200245c26144140cf71be43ab62
bharathivasan2002 <20ca019@sankaracollege.edu.in>
2025-05-31 16:27:45 +0530

    Updated homepage message

--- src/main/java/com/demohosting/demohosting/UserController.java
+++ src/main/java/com/demohosting/demohosting/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("user")
     public String getUser(){
-        return "hello userssss";
+        return "Hello, Render!";
     }
 }
